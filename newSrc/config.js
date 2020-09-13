6aa80a563cfa8bc50a6c35ae6ae52654fe620085
dan13ram <dan13ram@gmail.com>
2020-09-13 10:46:30 +0530

    basic setup

---
+++ newSrc/config.js
@@ -0,0 +1,11 @@
+export const CONFIG = {
+    networkId: 4,
+    marketAddress: "0x79520adff8e9c61401b4a8f930fa9de2f2a7d6fd",
+    priceAssets: [
+        {
+            address: "0xc3dbf84Abb494ce5199D5d4D815b10EC29529ff8",
+            symbol: "DAI"
+        }
+    ],
+    subgraph: "https://api.thegraph.com/subgraphs/name/shopfs/market-rinkeby"
+};
---
+++ newSrc/index.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import config from "config";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./helpers";
+import { createClient, Provider as GraphProvider } from "urql";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const client = createClient({ url: config.subgraph });
+
+const store = configureStore();
+ReactDOM.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <GraphProvider value={client}>
+                <App />
+            </GraphProvider>
+        </Provider>
+    </React.StrictMode>,
+    document.getElementById("root")
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
